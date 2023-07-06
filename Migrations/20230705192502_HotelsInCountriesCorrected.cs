fcc698ed1321bcb831975ade5f59421d7861273d
Polina Berest <pamapolya@gmail.com>
2023-07-06 19:53:32 +0200

    Added Countries Endpoint

---
+++ Migrations/20230705192502_HotelsInCountriesCorrected.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class HotelsInCountriesCorrected : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
